99458db29f2578f1988cf2f0195373a86cbfc99c
Gabriel Barros <gabriel.sbarros777@gmail.com>
2022-11-25 16:07:23 -0300

    initial commit

--- src/easy/TwoSum.java
+++ src/easy/TwoSum.java
@@ -14,4 +14,5 @@ public class TwoSum {
         }
         return twoSum;  
     }
+
 }
--- src/test/LeetCode.java
+++ src/test/LeetCode.java
@@ -5,9 +5,11 @@ import easy.TwoSum;
 public class LeetCode {
 
 	public static void main(String[] args) {
+
 		TwoSum test = new TwoSum();
 		int[] i = new int[] {2,4,11,3};
 		test.twoSum(i, 6);
+		
 	}
 
 }
