f94e82b4578d7db98eb79c70ca9f1d7d7bfc57dd
Gabriel Barros <gabriel.sbarros777@gmail.com>
2022-11-25 16:03:45 -0300

    1. TwoSum

---
+++ src/test/LeetCode.java
@@ -0,0 +1,13 @@
+package test;
+
+import easy.TwoSum;
+
+public class LeetCode {
+
+	public static void main(String[] args) {
+		TwoSum test = new TwoSum();
+		int[] i = new int[] {2,4,11,3};
+		test.twoSum(i, 6);
+	}
+
+}
